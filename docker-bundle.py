31de54258c2e4bda4252daca1384c68a6bd1f1cf
xbw <windworst@gmail.com>
2018-06-11 16:34:53 +0800

    pep8 format

--- docker-bundle.py
+++ docker-bundle.py
@@ -17,7 +17,7 @@ bundles_dir = 'bundles'
 version_code = '0.1.0'
 version_number = 1
 
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 
 config_path = os.path.expanduser(os.path.join('~', '.' + exe_name))
 file_source_list = 'sources.list'
@@ -35,16 +35,18 @@ config = {
     'upgrade_info_url': upgrade_info_url,
 }
 
+
 def write_config(config):
     f = open(config_file_path, 'w')
     f.write(json.dumps(config, indent=2))
     f.flush()
     f.close()
 
+
 def init_config_file():
     try:
         os.makedirs(config_path)
-    except:
+    except BaseException:
         pass
     try:
         if not os.path.isfile(source_list_file_path):
@@ -52,19 +54,20 @@ def init_config_file():
             sources_list.write(default_source)
             sources_list.flush()
             sources_list.close()
-    except:
+    except BaseException:
         print('[WARNING] Load sources list failed.')
     try:
         if not os.path.isfile(config_file_path):
             write_config(config)
         else:
             config.update(json.loads(open(config_file_path).read()))
-    except:
+    except BaseException:
         print('[WARNING] Load config file failed.')
 
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 
-def upgrade(user_call_update = False, update_with_ask = True):
+
+def upgrade(user_call_update=False, update_with_ask=True):
     skip_version_number = config['skip_version_number']
     if not user_call_update and skip_version_number.lower() == 'all':
         return
@@ -83,8 +86,11 @@ def upgrade(user_call_update = False, update_with_ask = True):
         'mode': 'normal',
     }
     try:
-        update_info.update(json.loads(urllib.request.urlopen(config['upgrade_info_url']).read()))
-    except:
+        update_info.update(
+            json.loads(
+                urllib.request.urlopen(
+                    config['upgrade_info_url']).read()))
+    except BaseException:
         return
 
     update_url = update_info['url']
@@ -98,29 +104,29 @@ def upgrade(user_call_update = False, update_with_ask = True):
 
     if int(update_version_number) <= version_number:
         if user_call_update:
-            print('-'*80)
-            print('     %s is newest version.'%exe_name)
-            print('-'*80)
+            print('-' * 80)
+            print('     %s is newest version.' % exe_name)
+            print('-' * 80)
         return
 
     is_ask_for_upgrade = update_mode == 'ask'
     is_silent_upgrade = update_mode == 'silent'
 
     if not user_call_update and not is_silent_upgrade\
-        and not is_ask_for_upgrade\
-        and skip_version_number >= str(update_version_number):
+            and not is_ask_for_upgrade\
+            and skip_version_number >= str(update_version_number):
         return
 
     if not is_silent_upgrade and (is_ask_for_upgrade or update_with_ask):
         answer = input(
-"""--------------------------------------------------------------------------------
+            """--------------------------------------------------------------------------------
     A newly version (%s) of %s is available. Upgrade?
             [Y]= Yes
             [N]= Not now
             [S]= Skip this version
             [D]= Disable upgrade
 --------------------------------------------------------------------------------
-    Answer: [Y]: """%(update_version_code,exe_name))[:1].upper()
+    Answer: [Y]: """ % (update_version_code, exe_name))[:1].upper()
         print()
 
         if answer == 'N':
@@ -141,24 +147,29 @@ def upgrade(user_call_update = False, update_with_ask = True):
                 print('[FAILED]         Upgrade data hash error!')
             return
 
-        f = open(__file__,'wb')
+        f = open(__file__, 'wb')
         f.write(new_exe)
         f.flush()
         f.close()
         if user_call_update or is_ask_for_upgrade:
-            print('='*80)
-            print('         %s (%s) is up to date in your next command.'%(project_name, update_version_code))
-            print('='*80)
-    except:
+            print('=' * 80)
+            print(
+                '         %s (%s) is up to date in your next command.' %
+                (project_name, update_version_code))
+            print('=' * 80)
+    except BaseException:
         if user_call_update or is_ask_for_upgrade:
-            print('[FAILED]         upgrade:',sys.exc_info()[1])
+            print('[FAILED]         upgrade:', sys.exc_info()[1])
             return
 
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+
 
 is_installed = False
 
 # import bundle.py
+
+
 def load_sub_bundle():
     current_path = os.getcwd()
     paths = []
@@ -184,11 +195,15 @@ def load_sub_bundle():
     return bundles
 
 # import from '__file__/bundles_dir'
-def load_bundles(bundle_dir_path = os.path.join(os.path.dirname(__file__), bundles_dir)):
+
+
+def load_bundles(bundle_dir_path=os.path.join(
+        os.path.dirname(__file__), bundles_dir)):
     bundles = {}
     try:
-        bundle_names = list(map(lambda x:x[0:-3], filter(lambda x:x[-3:]=='.py', os.listdir(bundle_dir_path))))
-    except:
+        bundle_names = list(map(
+            lambda x: x[0:-3], filter(lambda x: x[-3:] == '.py', os.listdir(bundle_dir_path))))
+    except BaseException:
         bundle_names = []
     if len(bundle_names) > 0:
         sys.path.insert(0, bundle_dir_path)
@@ -198,10 +213,13 @@ def load_bundles(bundle_dir_path = os.path.join(os.path.dirname(__file__), bundl
                 bundles.update(__import__(bundle).actions)
     return bundles
 
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+
 
 def load_source():
-    return set(filter(lambda x:x!='', map(lambda x:x.strip(), open(source_list_file_path).readlines())))
+    return set(filter(lambda x: x != '', map(
+        lambda x: x.strip(), open(source_list_file_path).readlines())))
+
 
 def write_source(sources):
     f = open(source_list_file_path, 'w')
@@ -211,10 +229,10 @@ def write_source(sources):
     f.close()
 
 
-def load_packages(sources = []):
+def load_packages(sources=[]):
     try:
         sources += load_source()
-    except:
+    except BaseException:
         pass
     if len(sources) == 0:
         sources = [default_source]
@@ -223,34 +241,43 @@ def load_packages(sources = []):
     for source in sources:
         try:
             packages.update(json.loads(urllib.request.urlopen(source).read()))
-        except:
+        except BaseException:
             pass
     return packages
 
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 # install
 
+
 def md5(data):
-    if type(data) == str:
+    if isinstance(data, str):
         data = data.encode()
     return hashlib.md5(data).hexdigest()
 
+
 def install_from_dir(package_path, target_path):
     try:
-        shutil.copytree(package_path, target_path, ignore=shutil.ignore_patterns('.git'))
+        shutil.copytree(
+            package_path,
+            target_path,
+            ignore=shutil.ignore_patterns('.git'))
         return True
-    except:
-        print('[ERROR]      Install from dir \'%s\' failed, %s.'%package_path, sys.exc_info()[1])
+    except BaseException:
+        print(
+            '[ERROR]      Install from dir \'%s\' failed, %s.' %
+            package_path, sys.exc_info()[1])
         return None
+
 
 def install_from_tarfile(package_path, target_path):
     try:
         t = tarfile.open(package_path, "r:gz")
         t.extractall(target_path)
         return True
-    except:
+    except BaseException:
         print('[ERROR]      Package illegal.')
         return None
+
 
 def install_from_url(package_name, package_path, target_path):
     file_name = package_name + '-' + md5(package_path) + '.bundle'
@@ -258,7 +285,7 @@ def install_from_url(package_name, package_path, target_path):
     download_path = os.path.join(download_dir, file_name)
     try:
         os.makedirs(download_dir)
-    except:
+    except BaseException:
         pass
     if os.path.isfile(download_path):
         print('[INFO]       Load from cache...')
@@ -266,32 +293,39 @@ def install_from_url(package_name, package_path, target_path):
     try:
         urllib.request.urlretrieve(package_path, download_path)
         return install_from_tarfile(download_path, target_path)
-    except:
-        print('[ERROR]      Download \'%s\' failed, %s.'%package_path, sys.exc_info()[1])
+    except BaseException:
+        print(
+            '[ERROR]      Download \'%s\' failed, %s.' %
+            package_path,
+            sys.exc_info()[1])
     return None
 
-def install_from_git(package_name, package_path, target_path, branch = 'master'):
+
+def install_from_git(package_name, package_path, target_path, branch='master'):
     file_name = package_name + '-' + md5(package_path) + '.bundle'
     download_dir = os.path.join(config_path, packages_dir)
     download_path = os.path.join(download_dir, file_name)
     success = False
     if os.path.isdir(download_path):
-        if 0 == os.system('cd "%s" && git checkout %s && git pull -f'%(download_path, branch)):
+        if 0 == os.system(
+                'cd "%s" && git checkout %s && git pull -f' % (download_path, branch)):
             success = True
         else:
             try:
                 shutil.rmtree(download_path)
-            except:
+            except BaseException:
                 pass
     if not success and not os.path.isdir(download_path):
-        if 0 == os.system("git clone --depth 1 \"%s\" \"%s\" -b %s"%(package_path, download_path, branch)):
+        if 0 == os.system("git clone --depth 1 \"%s\" \"%s\" -b %s" %
+                          (package_path, download_path, branch)):
             success = True
 
     if not success:
-        print('[ERROR]      Pull \'%s\' failed.'%package_path)
+        print('[ERROR]      Pull \'%s\' failed.' % package_path)
         return None
 
     return install_from_dir(download_path, target_path)
+
 
 def install_from_package(package_name, package_path, target_path):
     if package_path.find('http://') == 0 or package_path.find('https://') == 0:
@@ -301,13 +335,16 @@ def install_from_package(package_name, package_path, target_path):
     elif os.path.isfile(package_path):
         return install_from_tarfile(package_path, target_path)
     else:
-        print('[ERROR]      Can\'t find package \'%s\' in \'%s\''%(package_name, package_path))
+        print(
+            '[ERROR]      Can\'t find package \'%s\' in \'%s\'' %
+            (package_name, package_path))
         return None
 
-def install(argv = []):
+
+def install(argv=[]):
     def install_help():
         print(
-"""
+            """
 Usage:
     %(name)s install [option] <bundle-name>
 
@@ -317,7 +354,7 @@ Options:
 
 Description:
     <bundle-name>                           Run '%(name)s search <your bundle>' get bundle-names, or in '-p' for package path.
-"""%{'name': exe_name})
+""" % {'name': exe_name})
 
     opts, args = getopt.getopt(argv, 'ps:', ['package', 'source='])
 
@@ -328,7 +365,9 @@ Description:
     target_path = os.path.join(os.getcwd(), bundle_install_dir)
 
     if is_installed or os.path.isdir(target_path):
-        print('         %(project_name)s already installed, if want reinstall, manully delete \'docker\' folder first.'%{'project_name': project_name})
+        print(
+            '         %(project_name)s already installed, if want reinstall, manully delete \'docker\' folder first.' % {
+                'project_name': project_name})
         return
 
     bundle_name = args[0]
@@ -351,7 +390,7 @@ Description:
         packages = load_packages(use_sources)
 
         if bundle_name not in packages:
-            print("[ERROR]      Bundle '%s' not found."%(bundle_name))
+            print("[ERROR]      Bundle '%s' not found." % (bundle_name))
             return
 
         print('[INFO]       Bundle: ' + bundle_name)
@@ -363,9 +402,16 @@ Description:
         print('[INFO]       Package: ' + package_url)
 
         if package_type == 'git':
-            result = install_from_git(bundle_name, package_url, target_path, package_info.get('branch', 'master'))
+            result = install_from_git(
+                bundle_name,
+                package_url,
+                target_path,
+                package_info.get(
+                    'branch',
+                    'master'))
         elif not package_type:
-            result = install_from_package(bundle_name, package_url, target_path)
+            result = install_from_package(
+                bundle_name, package_url, target_path)
 
     if not result:
         print('[ERROR]      Install bundle failed..')
@@ -381,16 +427,17 @@ Description:
     loaded_actions = load_bundles(os.path.join(target_path, bundles_dir))
     print('You get new Bundle Commands:')
     for name, action in loaded_actions.items():
-        print("    %-30s%s"%(name, action['desc']))
+        print("    %-30s%s" % (name, action['desc']))
     print()
 
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 # search
 
-def search(argv = []):
+
+def search(argv=[]):
     def search_help():
         print(
-"""
+            """
 Usage:
     %(name)s search <keyword>
 
@@ -399,8 +446,10 @@ Options:
     -r, --regex                          Keyword use Regex
     -s, --source <source>                Find bundle from given source.
     -h, --help                           Show this.
-"""%{'name': exe_name})
-    opts, args = getopt.getopt(argv, 'nrs:h', ['name', 'regex', 'source=', 'help'])
+""" % {'name': exe_name})
+    opts, args = getopt.getopt(
+        argv, 'nrs:h', [
+            'name', 'regex', 'source=', 'help'])
 
     if len(args) == 0:
         search_help()
@@ -429,27 +478,26 @@ Options:
 
     def check(content):
         if use_regex:
-            return re_keyword.search(content) != None
+            return re_keyword.search(content) is not None
         else:
-            return content.find(keyword) >=0
+            return content.find(keyword) >= 0
 
-    print("%-40s%s"%('NAME', 'Description'))
+    print("%-40s%s" % ('NAME', 'Description'))
     for name, info in packages.items():
         desc = ''
         if 'desc' in info:
             desc = info['desc']
         if check(name) or not name_only and check(desc):
-            print("%-40s%s"%(name, desc))
+            print("%-40s%s" % (name, desc))
 
 
-
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 # source
 
-def source(argv = []):
+def source(argv=[]):
     def source_help():
         print(
-"""
+            """
 Usage:
     %(name)s source [OPTIONS]
 
@@ -458,8 +506,10 @@ Options:
     -r, --remove <source>                Remove Source
     -l, --list                           List sources.
     -h, --help                           Show this.
-"""%{'name': exe_name})
-    opts, args = getopt.getopt(argv, 'a:r:lh', ['add=', 'remove=', 'list', 'help'])
+""" % {'name': exe_name})
+    opts, args = getopt.getopt(
+        argv, 'a:r:lh', [
+            'add=', 'remove=', 'list', 'help'])
 
     if len(opts) == 0:
         source_help()
@@ -485,11 +535,12 @@ Options:
         write_source(sources)
 
 # import finish
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 
-def help(args = []):
+
+def help(args=[]):
     print(
-"""
+        """
     %(project_name)s
 
 Usage:
@@ -502,19 +553,20 @@ Options:
        --check-upgrade                      Check self upgrade before action
        --upgrade                            Do self upgrade directly (without ask) if upgrade available
 
-Commands:"""%{'name': exe_name, 'project_name': project_name})
+Commands:""" % {'name': exe_name, 'project_name': project_name})
     for name, action in base_actions.items():
-        print("    %-30s%s"%(name, action['desc']))
+        print("    %-30s%s" % (name, action['desc']))
     print()
     init_actions_bundles()
     if len(bundles) > 0:
         print("Bundle Commands:")
         for name, action in bundles.items():
-            print("    %-30s%s"%(name, action['desc']))
+            print("    %-30s%s" % (name, action['desc']))
         print()
     exit()
 
-def version(args = []):
+
+def version(args=[]):
     f = open(__file__)
     file_hash = md5(f.read())
     f.close()
@@ -526,44 +578,48 @@ def version(args = []):
     print(json.dumps(version_info, indent=2))
     exit()
 
-def environment(args = []):
+
+def environment(args=[]):
     for arg in args:
         a = arg.split('=')
         a.append(a[0])
-        k,v = a[:2]
+        k, v = a[:2]
         os.environ[k] = v
 
-def action_upgrade(args = []):
+
+def action_upgrade(args=[]):
     upgrade(True)
     exit()
 
 
-def action_upgrade_directly(args = []):
+def action_upgrade_directly(args=[]):
     upgrade(True, False)
     exit()
 
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 
 # command call actions
 
+
 base_actions = {
-        'install': {
-            'desc': 'Install bundle here',
-            'action': install
-            },
-        'search': {
-            'desc': 'Search for bundle you want',
-            'action': search
-            },
-        'source': {
-            'desc': 'Manage sources',
-            'action': source
-            },
-        }
+    'install': {
+        'desc': 'Install bundle here',
+        'action': install
+    },
+    'search': {
+        'desc': 'Search for bundle you want',
+        'action': search
+    },
+    'source': {
+        'desc': 'Manage sources',
+        'action': source
+    },
+}
 
 actions = {}
 
 bundles = {}
+
 
 def init_actions_bundles():
     actions.update(base_actions.copy())
@@ -573,7 +629,7 @@ def init_actions_bundles():
     actions.update(bundles)
 
 
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 
 def main():
     init_config_file()
@@ -589,7 +645,8 @@ def main():
         '--upgrade': action_upgrade_directly,
     }
 
-    opts, args = getopt.getopt(sys.argv[1:], 'hve:', ['help', 'version', 'environment=', 'check-upgrade', 'upgrade'])
+    opts, args = getopt.getopt(sys.argv[1:], 'hve:', [
+                               'help', 'version', 'environment=', 'check-upgrade', 'upgrade'])
 
     for opt in opts:
         options[opt[0]]([opt[1]])
@@ -603,17 +660,18 @@ def main():
     action = args[0]
 
     if action not in actions:
-        print("%s: command '%s' not found."%(exe_name, action))
+        print("%s: command '%s' not found." % (exe_name, action))
         return
     try:
         actions[action]['action'](args[1:])
     except getopt.GetoptError:
-        print('       %s:'%action, sys.exc_info()[1])
+        print('       %s:' % action, sys.exc_info()[1])
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     try:
         exit(main())
     except KeyboardInterrupt:
         pass
     except getopt.GetoptError:
-        print(exe_name, ':',sys.exc_info()[1])
+        print(exe_name, ':', sys.exc_info()[1])
